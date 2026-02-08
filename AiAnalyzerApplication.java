943c4d20633cde58054a645b9c7609329f4622a5
ranayagnadeep26-commits <ranayagnadeep26@gmail.com>
2026-02-08 21:12:32 +0530

    Add files via upload

---
+++ AiAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.cfs.aiAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAnalyzerApplication.class, args);
+	}
+
+}
